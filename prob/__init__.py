df287f000d1ded7c8c856d979d35af7cecd10eb4
mbel24 <beldamartina@gmail.com>
2025-12-16 11:39:07 -0500

    Update module docstring in __init__.py

--- prob/__init__.py
+++ prob/__init__.py
@@ -1,11 +1,11 @@
-"
+"""
 PROB: Pseudotime-based Gene Regulatory Network Inference Package
 
 Modules:
 - progression        : Diffusion pseudotime inference and smoothing
 - ode_bayesian_lasso : Bayesian Lasso ODE-based GRN inference
 - preprocess         : GEO data download, probe-to-gene mapping, normalization
-"
+"""
 
 from .progression import Progression_Inference
 from .ode_bayesian_lasso import ODE_BayesianLasso
